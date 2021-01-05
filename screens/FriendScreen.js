3e294f69bdd2f63e63029fbd8fff99e422a55b85
jwu23 <jasonwu1623@gmail.com>
2021-01-04 17:09:50 -0800

    Cleanup

--- screens/FriendScreen.js
+++ screens/FriendScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, TextInput, StyleSheet, TouchableOpacity, Modal, ScrollView } from 'react-native';
-import * as firebase from 'firebase';
-import Accordion from 'react-native-collapsible';
+import firebase from 'firebase';
+// import Accordion from 'react-native-collapsible';
 import { List } from 'react-native-paper';
 import { Ionicons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -16,7 +16,7 @@ export default class FriendScreen extends React.Component {
   }
 
   componentDidMount = () => {
-    console.log('friend list', this.props.route.params)
+    // console.log('friend list', this.props.route.params)
     firebase.database().ref('lists/' + this.props.route.params.user.uid).on('value', (snapshot) => {
       var lists = [];
       snapshot.forEach(child => {
@@ -33,7 +33,6 @@ export default class FriendScreen extends React.Component {
   mapItems = (listName) => {
     var lists = [];
     firebase.database().ref('lists/' + this.props.route.params.user.uid + `/${listName}`).on('value', (snapshot) => {
-      // console.log('val', snapshot)
       snapshot.forEach(child => {
         var temp = child.val();
         lists.push(
@@ -45,7 +44,6 @@ export default class FriendScreen extends React.Component {
     })
     return lists;
   }
-
 
   mapLists = () => {
     return this.state.userLists.map((name, index) => {
@@ -79,90 +77,23 @@ const styles = StyleSheet.create({
   },
   standaloneRowFront: {
     alignItems: 'center',
-    // backgroundColor: 'lightblue',
     justifyContent: 'center',
     height: 50,
   },
   individualList: {
     width: '100%',
     backgroundColor: '#63c96a',
-    // borderRadius: 25,
     borderWidth: 1,
     height: 50,
     alignItems: 'center',
     justifyContent: 'center',
-    // marginBottom: 10,
-    // marginTop: 20
   },
   container: {
     flex: 1,
-    // backgroundColor: 'lightblue',
-    // alignItems: 'flex-end',
-    // justifyContent: 'center',
   },
   container1: {
     backgroundColor: 'lightblue',
     alignItems: 'flex-end',
     justifyContent: 'center',
-  },
-  body: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'lightblue',
-  },
-  searchFriend: {
-    width: '100%',
-    backgroundColor: '#24962c',
-    borderRadius: 5,
-    height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingLeft: 25,
-    // marginRight: 10,
-    marginTop: 50
-  },
-  newItem: {
-    flex: 3.5,
-    backgroundColor: 'white',
-    borderRadius: 10,
-    padding: 5
-  },
-  addItemButton: {
-    flex: 1,
-    padding: 5,
-    backgroundColor: 'red',
-  },
-  modalView: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'lightblue',
-    marginBottom: '50%',
-    height: '100%',
-  },
-  inputView: {
-    width: '80%',
-    backgroundColor: 'white',
-    borderRadius: 25,
-    height: 50,
-    marginBottom: 20,
-    justifyContent: 'center',
-    padding: 20
-  },
-  itemInputView: {
-    flexDirection: 'row'
-  },
-  inputText: {
-    height: 50
-  },
-  newListButton: {
-    width: '80%',
-    backgroundColor: '#24962c',
-    borderRadius: 25,
-    height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 10,
-    marginTop: 20
   },
 })
