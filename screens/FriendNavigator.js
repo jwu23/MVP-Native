b3c99f7a0ec0a79ad11ee0be6ed43c8b49c497c0
jwu23 <jasonwu1623@gmail.com>
2021-01-04 17:07:45 -0800

    Add badge

--- screens/FriendNavigator.js
+++ screens/FriendNavigator.js
@@ -10,11 +10,15 @@ import FriendScreen from './FriendScreen.js';
 const Stack = createStackNavigator();
 
 export default class FriendNavigator extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <NavigationContainer independent={true}>
         <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen options={{headerShown: false}} name="Friends" component={UserFriends} />
+          <Stack.Screen options={{headerShown: false}} name="Friends" children={(navigation) => <UserFriends navigation={navigation} setNumRequests={this.props.setNumRequests}/>} />
           <Stack.Screen options={({route}) => ({title: route.params.user.name, headerStyle: { backgroundColor: 'orange' }})} name="Details" component={FriendScreen} />
         </Stack.Navigator>
       </NavigationContainer>
