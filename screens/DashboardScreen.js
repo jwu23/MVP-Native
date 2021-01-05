55e792509b158c64059aa2d84d9805e4b2ae9b89
jwu23 <jasonwu1623@gmail.com>
2021-01-04 17:06:50 -0800

    Cleanup

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,10 +1,4 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import * as firebase from 'firebase';
-
-import { createAppContainer, createMaterialTopTabNavigator } from 'react-navigation';
-import UserLists from './UserLists.js';
-import UserFriends from './UserFriends.js';
 import UserNavigator from './UserNavigator.js';
 
 export default class DashboardScreen extends React.Component {
@@ -15,22 +9,3 @@ export default class DashboardScreen extends React.Component {
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'lightblue',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logoutButton: {
-    width: '80%',
-    backgroundColor: 'green',
-    borderRadius: 25,
-    height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 10,
-    marginTop: 20
-  }
-})
